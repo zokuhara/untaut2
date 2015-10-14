7254182f906715721be524cb56e4ab1f7929d9bc
Zachary Okuhara <zacharyokuhara@Zacharys-MacBook-Pro.local>
2015-10-14 09:29:39 -0500

    Remove .js.js file

--- app/assets/javascripts/components/message.js.js
+++
@@ -1,9 +0,0 @@
-
-  render: function() {
-    return React.createElement("div", {className: "message"}, 
-      React.createElement("strong", null, this.props.message.user.username, ":"), "  ", this.props.message.body
-
-
-    );
-  }
-});
